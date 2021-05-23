38ffa7b98e044ef9001bf2659a29e93a5db13aa5
jaechung77 <jaechung77@gmail.com>
2021-05-24 02:26:20 +1000

    filter

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(drivers, string){
+    const newMatching = drivers.filter(function(driver){
+        return driver.toUpperCase() === string.toUpperCase()
+    })
+    return newMatching
+} 
+
+function fuzzyMatch(drivers, string){
+    const newMatching = drivers.filter(function(driver){
+        if (driver[0].toUpperCase() === string[0].toUpperCase())
+        return driver
+    })
+    return newMatching
+}
+
+function matchName(drivers, string){
+    const newMatching = drivers.filter(function(driver){
+        if (driver.name === string )
+        return driver.hometown
+    })  
+    return newMatching  
+}
